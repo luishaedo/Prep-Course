fd8befdf49a016d903ba9a93f51015959dc1f3ec
Luishaedo <luismariahaedolanda@gmail.com>
2022-08-18 20:22:57 -0300

    definicionesJSV

---
+++ 06-JS-V/homework/DefinicionesJSV.js
@@ -0,0 +1,8 @@
+/// DEFINICIONES JS V
+
+///PROTOTYPE: Son una manera por la que podemos definir propiedades y funciones a heredar por los objetos, sin tener que 
+///"cargarlos" en su definicion. Es decir, cada objeto es creado a traves de una plantilla, y hereda de esta propiedades 
+///sin haberlas definido en ellos literalmente. Esto permite "ahorrarse" al sistema mucha memoria, y al programador, muchas
+///lineas, ya que acude a las  funciones y propiedades de manera referencial. 
+
+///Constructores: Es un metodo o funcion especial para crear e inicializar un objerto creado a partir de una clase.
