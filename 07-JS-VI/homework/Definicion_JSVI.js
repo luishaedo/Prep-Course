ba63ce9eff950968a7dc707eaa04b9e1163233a7
Luishaedo <luismariahaedolanda@gmail.com>
2022-08-24 17:30:56 -0300

    JSVI

---
+++ 07-JS-VI/homework/Definicion_JSVI.js
@@ -0,0 +1,7 @@
+/// CALL BACK
+///
+//// Una funcion de call back es una funcion que se pasa a otra funcion como un argumento, que luego 
+//// se invoca dentro de la funcion externa para completar alguna accion.
+////
+
+
